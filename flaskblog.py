2809f9c5e736c3c8a5d245b3da8a2cca08af26b6
Rohit BM <rohit991812@gmail.com>
2021-03-02 00:13:39 +0530

    Jinja template engine + Bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+blog_posts = [
+    {
+     'author':'Rohit',
+     'title': 'GANs',
+     'content': 'intro to GANs',
+     'date_posted': '1st March 2021'
+    },
+
+    {
+     'author':'Toby',
+     'title': 'CNNs',
+     'content': 'intro to CNNs',
+     'date_posted': '2nd March 2021'
+    }
+]
+
 @app.route("/")
+@app.route("/home") 
 def func():
-    return "Hello world"
+    return render_template("home.html", posts=blog_posts ,title="Blog-Homepage")
 
 @app.route("/about")
 def about_page():
-    return "About page"
+    return render_template("about.html", title = "About-Us")
 
 @app.route("/contact")
 def contact_page():
-    return "contact page"
+    return render_template("contact.html", title = "Contact")
 
 if(__name__ == "__main__"):
     
