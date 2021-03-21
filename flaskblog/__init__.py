26b19cd2c56e3ee18743f16cdad77adc3e87cec4
Rohit BM <rohit991812@gmail.com>
2021-03-21 20:01:13 +0530

    added package structure to remove circular imports

---
+++ flaskblog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fbc0fa1f087697dfe11483079b0fa1b9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+from flaskblog import routes
